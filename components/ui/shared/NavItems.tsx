d58c9bf1220f7dd35dfd0540074cc0cfdb94e2ce
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-09 00:27:28 -0500

    features: added hero section and authentication system

---
+++ components/ui/shared/NavItems.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <li
+            key={link.label}
+            className={cn(
+              "flex-center p-medium-16 whitespace-nowrap",
+              isActive && "text-primary-500"
+            )}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default NavItems;
