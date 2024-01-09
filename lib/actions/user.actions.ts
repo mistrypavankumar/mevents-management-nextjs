4e321f441f8bd995e55e6452129c2eab0356be5c
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-09 01:42:37 -0500

    features: created database connection and webhooks

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,69 @@
+"use server";
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+import { revalidatePath } from "next/cache";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (err) {
+    handleError(err);
+  }
+};
+
+export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updateUser) throw new Error("User update failed");
+
+    return JSON.parse(JSON.stringify(updateUser));
+  } catch (err) {
+    handleError(err);
+  }
+};
+
+export const deleteUser = async (clerkId: string) => {
+  try {
+    await connectToDatabase();
+
+    // find the user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) throw new Error("User not found");
+
+    // unlink relationship
+    await Promise.all([
+      // update the 'events' collection to remove references to the user
+      Event.updateMany(
+        {
+          _id: { $in: userToDelete.events },
+        },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // update the orders collection to remove reference to the user
+
+      //need to write order collection
+    ]);
+
+    const deleteUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deleteUser ? JSON.parse(JSON.stringify(userToDelete._id)) : null;
+  } catch (err) {
+    handleError(err);
+  }
+};
