5dd3ae4eac2d14173d1d9d8c8897e0026b6ab5eb
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-10 01:47:03 -0500

    added: CURD opertion with clerk

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
 import { revalidatePath } from "next/cache";
+import Order from "../database/models/order.model";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -19,6 +20,19 @@ export const createUser = async (user: CreateUserParams) => {
   }
 };
 
+export const getUserById = async (userId: string) => {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found.");
+    return JSON.parse(JSON.stringify(user));
+  } catch (err) {
+    handleError(err);
+  }
+};
+
 export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
   try {
     await connectToDatabase();
@@ -27,9 +41,9 @@ export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
       new: true,
     });
 
-    if (!updateUser) throw new Error("User update failed");
+    if (!updatedUser) throw new Error("User update failed");
 
-    return JSON.parse(JSON.stringify(updateUser));
+    return JSON.parse(JSON.stringify(updatedUser));
   } catch (err) {
     handleError(err);
   }
@@ -54,9 +68,11 @@ export const deleteUser = async (clerkId: string) => {
         { $pull: { organizer: userToDelete._id } }
       ),
 
-      // update the orders collection to remove reference to the user
-
-      //need to write order collection
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
     ]);
 
     const deleteUser = await User.findByIdAndDelete(userToDelete._id);
