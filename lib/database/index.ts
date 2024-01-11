6215ea72d4d52998bd4b0776fb6e8f5b77576693
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-11 13:07:55 -0500

    updated

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,6 +5,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  console.log("Is called...");
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
@@ -12,7 +13,6 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "mevent",
       bufferCommands: false,
     });
 
